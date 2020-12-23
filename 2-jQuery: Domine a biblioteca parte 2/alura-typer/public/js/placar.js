213d2c9143d03e12858c6835b2487120e0c387f5
gustavobrod <gustavo.brod@outlook.com>
2020-12-23 20:41:54 -0300

    jQuey23-12-2020

---
+++ 2-jQuery: Domine a biblioteca parte 2/alura-typer/public/js/placar.js
@@ -0,0 +1,46 @@
+$("#botao-placar").click(mostraPlacar);
+
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Gustavo"
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.append(linha);
+}
+
+function novaLinha(usuario, palavras){
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+    
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(){
+    event.preventDefault();
+    var linha = $(this).parent().parent();
+    linha.fadeOut();
+    setTimeout(function(){
+        linha.remove(); 
+    },600);
+
+}
+
+function mostraPlacar(){
+    $(".placar").stop().slideToggle(300);
+}
